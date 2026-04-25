716533e1418ed7b5721c09fa896cb1dcd10d4098
salman <salmanibnriyaj@gmail.com>
2026-04-25 19:26:05 +0600

    loading jsx added

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+
+const loading = () => {
+    return (
+        <div className="flex justify-center items-center h-[60vh]">
+            <span className="loading loading-bars loading-xl"></span>
+
+        </div>
+    );
+};
+
+export default loading;
