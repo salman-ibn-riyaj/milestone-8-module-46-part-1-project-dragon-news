3ac474cfe1c20ecf6e6e9d48428159f637012873
salman <salmanibnriyaj@gmail.com>
2026-04-26 22:12:44 +0600

    again fixed

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGO_DB || '');
+const client = new MongoClient(process.env.MONGO_DB);
 const db = client.db("dragon-news");
 
 export const auth = betterAuth({
@@ -15,12 +15,12 @@ export const auth = betterAuth({
   }),
   socialProviders: {
         google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID || '', 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '' 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET 
         }, 
         github: { 
-            clientId: process.env.GITHUB_CLIENT_ID || '', 
-            clientSecret: process.env.GITHUB_CLIENT_SECRET || '' 
+            clientId: process.env.GITHUB_CLIENT_ID, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET 
         }, 
     },
     
